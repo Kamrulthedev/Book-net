1b212801884d9243ea4bfd61937de7c7f72ab0ce
Kamrul Hassan <kamrulthedev@gmail.com>
2024-09-23 22:36:42 +0600

    create-not-found-page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+
+const NotFoundPage = () => {
+    return (
+        <div className="text-center mt-48">
+            <h1 className="text-3xl font-serif">Not Found..!</h1>
+        </div>
+    );
+};
+
+export default NotFoundPage;
