8ff21009367b18bf5a6b127c311484ce364f45ed
Kamrul Hassan <kamrulthedev@gmail.com>
2024-09-23 11:42:25 +0600

    update-commit

--- src/services/PostService/index.ts
+++ src/services/PostService/index.ts
@@ -1,5 +1,5 @@
 export const getAllPosts = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/posts`);
 
   if (!res.ok) {
     throw new Error("Fetch Data Error");
